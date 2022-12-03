9c6d5f26e81004035892eabf21a0a270c9e5610b
Lucas Overland <lucas.overland@gmail.com>
2022-12-03 11:29:04 -0600

    Not everything needs to be one line

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -8,15 +8,12 @@ fn part_one(input: &str) -> usize {
         .lines()
         .map(|rucksack| {
             let compartments = rucksack.split_at(rucksack.len() / 2);
-            ALPHA_TO_DIGIT
-                .find(
-                    compartments
-                        .0
-                        .chars()
-                        .nth(compartments.0.find(|c| compartments.1.contains(c)).unwrap())
-                        .unwrap(),
-                )
-                .unwrap()
+            let common_item = compartments
+                .0
+                .chars()
+                .nth(compartments.0.find(|c| compartments.1.contains(c)).unwrap())
+                .unwrap();
+            ALPHA_TO_DIGIT.find(common_item).unwrap()
         })
         .sum()
 }
@@ -27,22 +24,16 @@ fn part_two(input: &str) -> usize {
         .chunks(3)
         .into_iter()
         .map(|group| {
-            ALPHA_TO_DIGIT
-                .find(
-                    group
-                        .fold(String::new(), |acc, elf| {
-                            if acc.len() == 0 {
-                                return elf.to_owned();
-                            }
-                            let acc_set: HashSet<char> = HashSet::from_iter(acc.chars());
-                            let elf_set: HashSet<char> = HashSet::from_iter(elf.chars());
+            let mut badge_letter = group.fold(String::new(), |acc, elf| {
+                if acc.len() == 0 {
+                    return elf.to_owned();
+                }
+                let acc_set: HashSet<char> = HashSet::from_iter(acc.chars());
+                let elf_set: HashSet<char> = HashSet::from_iter(elf.chars());
 
-                            acc_set.intersection(&elf_set).join("")
-                        })
-                        .pop()
-                        .unwrap(),
-                )
-                .unwrap()
+                acc_set.intersection(&elf_set).join("")
+            });
+            ALPHA_TO_DIGIT.find(badge_letter.pop().unwrap()).unwrap()
         })
         .sum()
 }
